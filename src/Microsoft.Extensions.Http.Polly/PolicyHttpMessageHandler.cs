ad8554ace8957a371523dad8395a220d4aec3f26
Ryan Nowak <nowakra@gmail.com>
2018-02-13 17:34:15 -0800

    Add a 'hello world' Polly handler

---
+++ src/Microsoft.Extensions.Http.Polly/PolicyHttpMessageHandler.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Polly;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.Http
+{
+    public class PolicyHttpMessageHandler : DelegatingHandler
+    {
+        private readonly IAsyncPolicy<HttpResponseMessage> _policy;
+
+        public PolicyHttpMessageHandler(IAsyncPolicy<HttpResponseMessage> policy)
+        {
+            if (policy == null)
+            {
+                throw new ArgumentNullException(nameof(policy));
+            }
+
+            _policy = policy;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            return _policy.ExecuteAsync(() => base.SendAsync(request, cancellationToken));
+        }
+    }
+}
